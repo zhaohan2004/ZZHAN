2e9ba3eea8d1161c6fa6ff2fdaf474862fb4573f
23107 <2310719709@qq.com>
2026-09-16 15:36:48 +0800

    修改访问路径

--- ZZHAN_web/admin/vite.config.ts
+++ ZZHAN_web/admin/vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig(({ command }) => ({
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
   server: {
     port: 8080,
-    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:9900',
--- ZZHAN_web/user/vite.config.ts
+++ ZZHAN_web/user/vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
   server: {
     port: 3000,
-    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:9900',
